5ae3d6021899bc8ecd42e4f035a429754cb7ac2f
iriny <irinya@gmail.com>
2019-09-28 13:02:45 -0500

    Added shared variable due to issues with extending. Can now add new faculty.

---
+++ Class_Example/src/sample/shared_variables.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class shared_variables {
+    public static ObservableList<UHEmployee> employeeList = FXCollections.observableArrayList();
+
+}
