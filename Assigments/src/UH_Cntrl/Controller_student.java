e9b370e91b866a4851632ace9c8b0e8afd1c7414
iriny <irinya@gmail.com>
2019-10-12 09:19:13 -0500

    Fine tuned filters

--- Assigments/src/UH_Cntrl/Controller_student.java
+++ Assigments/src/UH_Cntrl/Controller_student.java
@@ -1,8 +1,11 @@
 package UH_Cntrl;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.fxml.Initializable;
@@ -26,16 +29,18 @@ public class Controller_student implements Initializable  {
        col_gpa.setCellValueFactory(new PropertyValueFactory<Student,Float>("gpa"));
        col_major.setCellValueFactory(new PropertyValueFactory<Student,String>("major"));
 
-        populateTable();
+       populateTable();
+       pop_majors ();
+
+
     }
 
 
     @FXML
     public TableColumn col_name, col_age, col_gpa, col_major;
     public TableView student_tableview;
-
-
-
+    public ComboBox major_filter;
+    public JFXTextField gpa1, gpa2, age1, age2;
 
 
     //Connect to SQLite DB
@@ -60,6 +65,83 @@ public class Controller_student implements Initializable  {
         if (conn != null) {
             try {
                 Statement stmt = conn.createStatement();
+                ResultSet result_set = stmt.executeQuery(sql);
+                while (result_set.next()) {
+                    student_table_data.add(new Student(result_set.getString(1), result_set.getInt(2)
+                            , result_set.getFloat(3), result_set.getString(4)));
+                }
+                student_tableview.setItems(student_table_data);
+                conn.close();
+            } catch (SQLException tableQueryException) {
+                System.err.println(tableQueryException.toString());
+            }
+        }
+    }
+
+    public void pop_majors () {
+        //Set Major ComboBox from DB
+        Connection conn = this.connect();
+        ObservableList<String> majors_list = FXCollections.observableArrayList();
+        String sql = "SELECT DISTINCT Major FROM Students";
+        majors_list.add("All");
+
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
+                ResultSet result_set = stmt.executeQuery(sql);
+                while (result_set.next()) {
+                    String M = (result_set.getString("Major"));
+                    majors_list.add(M);
+                }
+
+                major_filter.setItems(majors_list);
+                System.out.println(majors_list);
+
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void filter_majors (){
+        Connection conn = this.connect();
+        student_table_data = FXCollections.observableArrayList();
+        String F_major = String.valueOf(major_filter.getValue());
+        String sql = "SELECT * FROM Students WHERE Major = " + "'" + F_major + "'";
+
+        if (major_filter.getValue() == "All") {
+            populateTable();
+        } //Resets major filter
+
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
+                ResultSet result_set = stmt.executeQuery(sql);
+                while (result_set.next()) {
+                    student_table_data.add(new Student(result_set.getString(1), result_set.getInt(2)
+                            , result_set.getFloat(3), result_set.getString(4)));
+                }
+                student_tableview.setItems(student_table_data);
+                conn.close();
+            } catch (SQLException tableQueryException) {
+                System.err.println(tableQueryException.toString());
+            }
+        }
+    }
+
+
+
+    public void filter_age () {
+        student_table_data = FXCollections.observableArrayList();
+
+        int age_from = Integer.parseInt(String.valueOf(age1.getText()));
+        int age_to = Integer.parseInt(String.valueOf(age2.getText()));
+
+        Connection conn = this.connect();
+        String sql = "SELECT * FROM students WHERE age >= " + age_from + " AND age <= " + age_to;
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
                 ResultSet result_set = stmt.executeQuery(sql); //
                 while (result_set.next()) {
                     student_table_data.add(new Student(result_set.getString(1), result_set.getInt(2)
@@ -67,12 +149,37 @@ public class Controller_student implements Initializable  {
                 }
                 student_tableview.setItems(student_table_data);
 
+                conn.close();
+            } catch (SQLException tableQueryException) {
+                System.err.println(tableQueryException.toString());
+            }
+        }
+    }
+
+    public void filter_gpa () {
+        student_table_data = FXCollections.observableArrayList();
+
+        int gpa_from = Integer.parseInt(String.valueOf(gpa1.getText()));
+        int gpa_to = Integer.parseInt(String.valueOf(gpa2.getText()));
+
+        Connection conn = this.connect();
+        String sql = "SELECT * FROM Students WHERE gpa >= " + gpa_from + " AND gpa <= " + gpa_to;
+        if (conn != null) {
+            try {
+                Statement stmt = conn.createStatement();
+                ResultSet result_set = stmt.executeQuery(sql); //
+                while (result_set.next()) {
+                    student_table_data.add(new Student(result_set.getString(1), result_set.getInt(2)
+                            , result_set.getFloat(3), result_set.getString(4)));
+                }
+                student_tableview.setItems(student_table_data);
 
                 conn.close();
             } catch (SQLException tableQueryException) {
                 System.err.println(tableQueryException.toString());
             }
-            System.out.println("ok");
         }
     }
+
+
 }
